ed5631ac4943e7ecc8a00bb1dc0738591b0d8d6e
Bjoern70 <bjoern.foren@ewetel.net>
2024-06-12 01:15:02 +0200

    Fixed deadlock bug after filament runout and resume. Fixed pause deadlock bug.

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-//#define SHORT_BUILD_VERSION "v1.14.1"
+//#define SHORT_BUILD_VERSION "v1.14.2"
 
 /**
  * Verbose version identifier which should contain a reference to the location
--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "v1.14.1"
+  #define SHORT_BUILD_VERSION "v1.14.2"
 #endif
 
 /**
